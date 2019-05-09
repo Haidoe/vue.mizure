8144476cc5b7e21fc172c65140578c8888902ae4
Haidren Amalia <haidren.amalia@gmail.com>
2019-05-10 05:08:09 +0800

    bootstrap vue

---
+++ src/components/HomeSection.vue
@@ -0,0 +1,35 @@
+<template>
+    <section id="home">
+        <div class="primary-overlay d-flex">
+            <div class="container">
+                <div class="row h-100">
+                    <div class="col-sm-12 col-lg-6 text-white text-center d-flex flex-column align-items-center justify-content-center">
+                        <h1 class="display-3 mb-3 home-title"> Do What You Dream Of ... </h1>
+                        <p class="lead">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa, explicabo!</p>
+                        <button class="btn btn-outline-secondary text-white btn-lg">
+                        <i class="fas fa-arrow-right"></i>
+                        Read more
+                        </button>
+                    </div>
+
+                    <div class="col-lg-6">
+                        <img src="../assets/img/book.png" alt="" class="img-fluid d-none d-lg-block">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+
+<style lang="scss" scoped>
+#home {
+    background-image: url("../assets/img/mountains.jpg");
+    min-height: 600px;
+}
+
+#home .primary-overlay {
+    background: rgba(50, 146, 166, 0.8);
+    min-height: 600px;
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,32 @@
+<template>
+    <b-navbar toggleable="lg" fixed="top" type="light" class="py-4">
+        <b-container>
+            <b-navbar-brand href="#">
+                <img src="../assets/img/haidoe-logo.svg" alt="logo-main" width="200px">
+            </b-navbar-brand>
+
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav class="ml-auto">
+                    <b-nav-item href="#home"> Home </b-nav-item>
+                    <b-nav-item href="#about"> About </b-nav-item>
+                    <b-nav-item href="#authors"> Meet the Authors </b-nav-item>
+                    <b-nav-item href="#contact"> Contact </b-nav-item>
+                </b-navbar-nav>
+            </b-collapse>
+        </b-container>
+    </b-navbar>
+</template>
+
+<style lang="scss" scoped>
+.navbar {
+    box-shadow: 2px 2px 5px #3292a6;
+    background: rgba(255, 255, 255, 0.9);
+}
+
+.nav-item {
+    font-size: 1.4rem;
+    padding-right: 1.4rem;
+}
+</style>
